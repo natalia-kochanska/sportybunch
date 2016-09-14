a008630f7838382768b93e41bafdb39af03c979d
n"natalia-kochanska <wczorajszy_zgon@wp.pl>
2016-09-14 15:58:34 +0800

    first

---
+++ db/migrate/20160914074317_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :surname
+      t.string :about
+      t.string :sport
+      t.string :town
+      t.date :dob
+
+      t.timestamps null: false
+    end
+  end
+end
